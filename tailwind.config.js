cea3e774987076ea72f7afaec462be714b57d621
AristideI <a.isingizwe@alustudent.com>
2023-08-26 14:12:10 +0200

    Latest Update

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        mainred: "#dc3545",
+        hoverred: "#b01726",
+        maingray: "#444"
+      }
+    },
+  },
+  plugins: [],
+}
+
